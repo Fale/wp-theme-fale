eccc13bfbf4ab30e145b2d45fb5e98fc68d4e5bb
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-21 14:51:46 +0200

    Add publisher tag

--- header.php
+++ header.php
@@ -49,6 +49,7 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="author" href="https://plus.google.com/100271380639248552621"  />
+<link rel="publisher" href="https://plus.google.com/100271380639248552621"  />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
